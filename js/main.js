e26189565271c45c3ff7219ebf701f171a1184b2
saidmoussadek <saiddus24@gmail.com>
2022-07-22 09:28:36 +0200

    push

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+      Url    : 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
+     
+    }
+   
+        {
+          
+          "Url": 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
+        } 
+   
+  })
